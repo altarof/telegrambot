b27dea9b728eb2831e8e481ccc99fb52ee492f62
altarof <altarof@gmail.com>
2023-11-16 14:59:24 +0700

    Create user_manager.py

---
+++ user_manager.py
@@ -0,0 +1,18 @@
+# 用户管理模块
+
+# 示例函数：添加新用户
+def add_user(user_data):
+    # 实现添加用户逻辑
+    pass
+
+# 示例函数：更新用户信息
+def update_user(user_id, update_data):
+    # 实现更新用户信息逻辑
+    pass
+
+# 示例函数：检查用户权限
+def check_user_permission(user_id, permission):
+    # 实现检查用户权限逻辑
+    pass
+
+
